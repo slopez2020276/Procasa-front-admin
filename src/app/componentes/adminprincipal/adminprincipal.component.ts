a4292341f42d70cddf724d462251bc2aae840668
AndruDeveloper001 <Anderson100JS@gmail.com>
2023-12-29 15:09:31 -0600

    Frontend Updated Admin, Public

--- src/app/componentes/adminprincipal/adminprincipal.component.ts
+++ src/app/componentes/adminprincipal/adminprincipal.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrl: './adminprincipal.component.css'
 })
 export class AdminprincipalComponent {
+  InputFile() {
+    const inputfile: any = (document.getElementById('file-portada') as HTMLInputElement | null)?.value;
+    document.getElementById('file-portada')?.setAttribute('data-content', inputfile);
+    document.getElementById('preview-portada')?.removeAttribute('src');
+    document.getElementById('preview-portada')?.setAttribute('src', "../../../assets/img/"+inputfile.slice(12));
+    console.log(inputfile.slice(12));
+}
+
+  DeleteImg(){
+  document.getElementById('preview-portada')?.removeAttribute('src');
+}
+
 
 }
