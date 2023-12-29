757e5563818e63e91c3a16fae7f7cd72451d59ee
AndruDeveloper001 <Anderson100JS@gmail.com>
2023-12-29 16:53:46 -0600

    Last Update Frontend

--- src/app/componentes/admin-navbar/admin-navbar.component.ts
+++ src/app/componentes/admin-navbar/admin-navbar.component.ts
@@ -8,7 +8,15 @@ import { Component } from '@angular/core';
 export class AdminNavbarComponent {
   MenuShow(){
     document.getElementById('container-menu')?.classList.toggle('openmenu');
-  }
-  
 
+
+    const bars = document.getElementsByClassName('bar');
+
+    for(let e = 0; e<bars.length; e++){
+      bars[e].classList.toggle('fx');
+      setTimeout(function(){
+          bars[e].classList.toggle('fx');
+        }, 300);
+    }
+  }
 }
