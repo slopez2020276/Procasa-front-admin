4e99c4db6a3daa79b7c6671255690288917b1510
AndruDeveloper001 <Anderson100JS@gmail.com>
2024-01-02 09:30:58 -0600

    update admin principal timeline

--- src/app/componentes/flecha/flecha.component.ts
+++ src/app/componentes/flecha/flecha.component.ts
@@ -6,6 +6,20 @@ import { Component } from '@angular/core';
   styleUrl: './flecha.component.css'
 })
 export class FlechaComponent {
+
+  constructor(){
+    let scrollywindow:number = 0;
+    window.addEventListener('scroll',function(){
+            scrollywindow=this.scrollY;
+          if(scrollywindow>=200){
+            document.getElementById('to-home-scroll')?.classList.add('toshow');
+          }else{
+            document.getElementById('to-home-scroll')?.classList.remove('toshow');
+            }
+    },false);
+}
+
+
   toHome(){
     document.getElementById("body")?.scrollIntoView({behavior:"smooth"});
   }
