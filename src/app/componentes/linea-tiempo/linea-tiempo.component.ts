2d07a5c46cc4ea1cd107257a41a9a677d10aff3e
sergio lopez~ <88870495+slopez2020276@users.noreply.github.com>
2024-01-02 16:58:02 -0600

    Linea de tiempo Funcional

--- src/app/componentes/linea-tiempo/linea-tiempo.component.ts
+++ src/app/componentes/linea-tiempo/linea-tiempo.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { LineaTiempoService } from '../../services/linea-tiempo.service';
 
 @Component({
   selector: 'app-linea-tiempo',
   templateUrl: './linea-tiempo.component.html',
   styleUrl: './linea-tiempo.component.css'
 })
-export class LineaTiempoComponent {
 
+export class LineaTiempoComponent implements OnInit {
+
+  lineaService = inject(LineaTiempoService)
+
+  async ngOnInit(){
+    const response = await this.lineaService.obtenerLineaTiempo()
+    this.data = response.lineFiended
+    console.log(this.data)
+  }
+
+  data:any
+
+  onMouseOver(osName:string): void{
+    console.log(osName)
+  }
+  
 }
+
--- src/app/services/linea-tiempo.service.ts
+++ src/app/services/linea-tiempo.service.ts
@@ -1,9 +1,37 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LineaTiempoService {
+  private httpClient = inject(HttpClient)
+  private baseUrl: string; 
 
-  constructor() { }
+  constructor() { 
+    this.baseUrl = 'http://localhost:3002/api'
+  }
+
+
+  obtenerLineaTiempo(){
+    return firstValueFrom(
+      this.httpClient.get<any>(`${this.baseUrl}/mostrarLineaTiempo`)
+    )
+  }
+
+  obtenerLineaxID(idLinea:any){
+
+  }
+  
+
+  createHeaders(){
+    return   {
+     headers: new HttpHeaders ({
+       'Authorization': localStorage.getItem('token')!
+     })
+   }
+ }
 }
+
+
