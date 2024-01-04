7c26ae75cd5c67c265cdb0a4f35bd0c448cab3bb
sergio lopez~ <88870495+slopez2020276@users.noreply.github.com>
2024-01-04 16:58:38 -0600

    ultimo

--- src/app/componentes/noticias/noticias.component.ts
+++ src/app/componentes/noticias/noticias.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { MisionService } from '../../services/mision.service';
 
 @Component({
   selector: 'app-noticias',
   templateUrl: './noticias.component.html',
   styleUrl: './noticias.component.css'
 })
-export class NoticiasComponent {
+
+
+export class NoticiasComponent implements OnInit{
+  data
+  enlace :string = ''
+  noticiasServices = inject(MisionService)
+  descricion: string = ''
+
+  async ngOnInit()  {
+    const response = await this.noticiasServices.obtenerMsion()
+    this.data = response.Mision
+   
+
+    console.log(this.data)
+    console.log(this.enlace)
+
+
+
+}
 
 }
