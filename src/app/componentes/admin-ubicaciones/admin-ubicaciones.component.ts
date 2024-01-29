b499b84d7a38e6c65dc146f6df8bf43c0043e96e
sergio lopez~ <88870495+slopez2020276@users.noreply.github.com>
2024-01-29 15:14:23 -0600

    ubicaciones

--- src/app/componentes/admin-ubicaciones/admin-ubicaciones.component.ts
+++ src/app/componentes/admin-ubicaciones/admin-ubicaciones.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { UbicacionServiceService } from '../../services/ubicacion-service.service';
 
 @Component({
   selector: 'app-admin-ubicaciones',
   templateUrl: './admin-ubicaciones.component.html',
   styleUrl: './admin-ubicaciones.component.css'
 })
-export class AdminUbicacionesComponent {
+export class AdminUbicacionesComponent implements OnInit {
+
+ubicacionService = inject(UbicacionServiceService)
+
+dataUbicacion
+
+
+
+ngOnInit(): void {
+  this.obtenerUbicacion()
+}
+
+async obtenerUbicacion(){
+  const ubicacion = await this.ubicacionService.ObtenerUbicaionesAdmin()
+  console.log(ubicacion.ubi)
+  this.dataUbicacion = ubicacion.ubi
+}
 
 
   AddLocation(){
