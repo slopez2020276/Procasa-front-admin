0bc6e7537570e138bfd32c5da064e0cf1bf044cf
AndruDeveloper001 <Anderson100JS@gmail.com>
2024-02-05 11:00:57 -0600

    estdaos ubicaciones

--- src/app/componentes/ubicaciones/ubicaciones.component.ts
+++ src/app/componentes/ubicaciones/ubicaciones.component.ts
@@ -15,27 +15,43 @@ export class UbicacionesComponent {
   data
   imgPrincipal
   imgFondo
- HistoriaService = inject(HistoriaService)
+  HistoriaService = inject(HistoriaService)
 
   async onSubmit(){ }
 
  async ngOnInit() {
-  document.getElementById('cont-slide')?.classList.add('a')
-    const response = await this.HistoriaService.obtenerHistoria()
+   document.getElementById('cont-slide')?.classList.add('a')
+   const response = await this.HistoriaService.obtenerHistoria()
+   document.getElementsByClassName('title-sucs')[0]?.classList.add('select')
+   const elements = document.getElementsByClassName('title-sucs') as HTMLCollectionOf<HTMLElement>; elements[0].style.color = '#CC0'
+
+
     this.data = response.historia
     this.imgPrincipal = this.data[0].imgPathPrincipal
     this.imgFondo = this.data[0].imgPathFondo
   }
 
 
-ShowContSucursales(c:any){
+ShowContSucursales(c:any, h:number){
   this.removeClass()
     document.getElementById('cont-slide')?.classList.add(c)
-}
-removeClass(){
-  document.getElementById('cont-slide')?.classList.remove('a')
-  document.getElementById('cont-slide')?.classList.remove('b')
-  document.getElementById('cont-slide')?.classList.remove('c')
+    const elements = document.getElementsByClassName('title-sucs') as HTMLCollectionOf<HTMLElement>
+    elements[h].style.color = '#CC0'
+
+    console.log(document.getElementsByClassName('title-sucs')[h])
+
+  }
+  removeClass(){
+    document.getElementById('cont-slide')?.classList.remove('a')
+    document.getElementById('cont-slide')?.classList.remove('b')
+    document.getElementById('cont-slide')?.classList.remove('c')
+      document.getElementsByClassName('title-sucs')[0]?.classList.remove('select')
+      document.getElementsByClassName('title-sucs')[1]?.classList.remove('select')
+      document.getElementsByClassName('title-sucs')[2]?.classList.remove('select')
+    const elements = document.getElementsByClassName('title-sucs') as HTMLCollectionOf<HTMLElement>
+    elements[0].style.color = '#FFF'
+    elements[1].style.color = '#FFF'
+    elements[2].style.color = '#FFF'
 }
 
 
