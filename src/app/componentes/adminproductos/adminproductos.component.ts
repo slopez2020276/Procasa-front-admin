ab4ab3e280e88547c695a6b3b5513c28c152503e
AndruDeveloper001 <Anderson100JS@gmail.com>
2024-02-15 15:26:33 -0600

    ...

--- src/app/componentes/adminproductos/adminproductos.component.ts
+++ src/app/componentes/adminproductos/adminproductos.component.ts
@@ -50,9 +50,54 @@ if (saveButtonTL) {
 
 }, false) }
 
-        document.getElementsByClassName('innerdetails')[2]!.innerHTML = sizemedida
-        document.getElementsByClassName('innerdetails')[0]!.innerHTML = imagen.width + " px"
-        document.getElementsByClassName('innerdetails')[1]!.innerHTML = imagen.height + " px"
+document.getElementsByClassName('innerdetails')[4]!.innerHTML = imagen.width + " px"
+document.getElementsByClassName('innerdetails')[5]!.innerHTML = imagen.height + " px"
+document.getElementsByClassName('innerdetails')[7]!.innerHTML = sizemedida
+        // document.getElementById('new-file-input')?.setAttribute('data-content', fileName)
+        this.inputEmptyEdit = fileName
+        document.getElementById(idfile)?.setAttribute('src', img.src)
+      }
+    }
+    lector.readAsDataURL(archivo)
+  }
+}
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+FileAdd(event: Event, idfile:string): void  {
+
+  const fileInput = event.target as HTMLInputElement
+  const archivo = fileInput.files?.[0]
+
+  if (archivo) { const lector = new FileReader()
+
+    lector.onload = (eventoLectura:any) => {
+      const imagen = new Image()
+      imagen.src = eventoLectura.target.result as string
+
+      imagen.onload = () => {
+        const fileSize: number = archivo.size
+        const size: any = fileSize.toFixed(2)
+        let medida: string
+        let sizemedida: any
+        if((size/1024/1024) < 1.0) {medida = " KB"; sizemedida = (size/1024).toFixed(2).toString() + medida }else{ medida = " MB"; sizemedida = (size/1024/1024).toFixed(2).toString() + medida }
+        const fileName: string = archivo.name
+        let img = new Image()
+        const objectURL = URL.createObjectURL(archivo)
+        img.src = objectURL
+
+const saveButtonTL = document.getElementById('save-new-tili')
+if (saveButtonTL) {
+  saveButtonTL.addEventListener('click', () => {
+
+}, false) }
+
+document.getElementsByClassName('innerdetails')[0]!.innerHTML = imagen.width + " px"
+document.getElementsByClassName('innerdetails')[1]!.innerHTML = imagen.height + " px"
+document.getElementsByClassName('innerdetails')[3]!.innerHTML = sizemedida
         // document.getElementById('new-file-input')?.setAttribute('data-content', fileName)
         this.inputEmpty = fileName
         document.getElementById(idfile)?.setAttribute('src', img.src)
@@ -61,6 +106,10 @@ if (saveButtonTL) {
     lector.readAsDataURL(archivo)
   }
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 
 
 generarSub() { const newSubIndex = this.subsArray.length + 1; this.subsArray.push(newSubIndex) }
