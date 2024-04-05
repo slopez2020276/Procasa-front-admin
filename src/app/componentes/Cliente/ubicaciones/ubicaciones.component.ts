94341e29f12f4287a323664d9e240b2792b5355b
AndruDeveloper001 <Anderson100JS@gmail.com>
2024-04-05 13:00:36 -0600

    cambios web public

--- src/app/componentes/Cliente/ubicaciones/ubicaciones.component.ts
+++ src/app/componentes/Cliente/ubicaciones/ubicaciones.component.ts
@@ -62,37 +62,22 @@ console.log(respuesta, this.dataUbicaciones)
   }
 
 
-ShowContSucursales(c:any, h:number){
-  this.removeClass()
-    document.getElementById('cont-slide')?.classList.add(c)
-    const elements = document.getElementsByClassName('title-sucs') as HTMLCollectionOf<HTMLElement>
-    elements[h].style.background = '#656565'
-
-
-
-
-
-
-  }
-
   showfilterImg(tipoTienda:string){
     this.filtroIMG = tipoTienda
     console.log(this.filtroIMG)
 
   }
-  removeClass(){
-    document.getElementById('cont-slide')?.classList.remove('a')
-    document.getElementById('cont-slide')?.classList.remove('b')
-    document.getElementById('cont-slide')?.classList.remove('c')
-      document.getElementsByClassName('title-sucs')[0]?.classList.remove('select')
-      document.getElementsByClassName('title-sucs')[1]?.classList.remove('select')
-      document.getElementsByClassName('title-sucs')[2]?.classList.remove('select')
-    const elements = document.getElementsByClassName('title-sucs') as HTMLCollectionOf<HTMLElement>
-    elements[0].style.background = '#252525'
-    elements[1].style.background = '#252525'
-    elements[2].style.background = '#252525'
-}
+ 
 
+checkLogo(item: number) {
+  const btns:HTMLElement | any = document.getElementsByClassName('cont-check')
+  if(btns){
+    for(let i=0; i<=btns.length; i++){
+      btns[i]?.classList.remove('enabled')
+      btns[item]?.classList.add('enabled')
+    }
+  }
+}
 
 
 
