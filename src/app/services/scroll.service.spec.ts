863d14da6f09e4e8e8f63dad2219edc5b5903181
AndruDeveloper001 <Anderson100JS@gmail.com>
2024-04-18 11:58:45 -0600

    .

--- src/app/services/scroll.service.spec.ts
+++ src/app/services/scroll.service.spec.ts
@@ -1,16 +1,1 @@
 import { TestBed } from '@angular/core/testing';
-
-import { ScrollService } from './scroll.service';
-
-describe('ScrollService', () => {
-  let service: ScrollService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ScrollService);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
