0487bed8da57396b879697bb60fc0a57c852bc29
AndruDeveloper001 <Anderson100JS@gmail.com>
2024-05-10 09:50:37 -0600

    Routing, Admin, Module, No Admin.  Fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,28 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { PrincipalComponent } from './componentes/Cliente/principal/principal.component';
 import { UbicacionesComponent } from './componentes/Cliente/ubicaciones/ubicaciones.component';
 import { QuieroComprarComponent } from './componentes/Cliente/quiero-comprar/quiero-comprar.component';
-import { AdminloginComponent } from './componentes/admin/adminlogin/adminlogin.component';
-import { AdminUbicacionesComponent } from './componentes/admin/admin-ubicaciones/admin-ubicaciones.component';
-import { AdminprincipalComponent } from './componentes/admin/adminprincipal/adminprincipal.component';
-import { PromocionesComponent } from './componentes/admin/promociones/promociones.component';
 import { AlertComponent } from './componentes/Complements/alert/alert.component';
 import { SuscripcionesComponent } from './componentes/Cliente/suscripciones/suscripciones.component';
-import { YoutubeComponent } from './componentes/Complements/youtube/youtube.component';
-import { UsuariosComponent } from './componentes/admin/usuarios/usuarios.component';
 import { NoticiasComponent } from './componentes/Cliente/noticias/noticias.component';
 import { NuestrosClientesComponent } from './componentes/Cliente/nuestros-clientes/nuestros-clientes.component';
 import { ProductosComponent } from './componentes/Cliente/productos/productos.component';
 import { NuestroEquipoComponent } from './componentes/Cliente/nuestro-equipo/nuestro-equipo.component';
-import { AdminproductosComponent } from './componentes/admin/adminproductos/adminproductos.component';
-import { EquipoComponent } from './componentes/admin/equipo/equipo.component';
 import { NoticiaUnicaComponent } from './componentes/Cliente/noticia-unica/noticia-unica.component';
-import { GeneralesComponent } from './componentes/admin/generales/generales.component';
-import { NuestrasMarcasComponent } from './componentes/admin/nuestras-marcas/nuestras-marcas.component';
 import { EquipoUnicoComponent } from './componentes/Cliente/equipo-unico/equipo-unico.component';
 import { loginGuard } from './guards/login.guard';
 import { agenteMerting } from './guards/agenteMerting.guard';
 import { NoticiasMovilComponent } from './componentes/Cliente/noticias-movil/noticias-movil.component';
-import { NuestrosClienteComponent } from './componentes/admin/nuestros-cliente/nuestros-cliente.component';
 import { agenteRH } from './guards/rh.guard';
 
 const routes: Routes = [
@@ -32,27 +21,16 @@ const routes: Routes = [
   {path:'Inicio',title:'PROCASA',component:PrincipalComponent},
   {path:'ubicaciones',title:'PROCASA | Ubicaciones',component:UbicacionesComponent},
   {path:'quiero-comprar',title:'PROCASA | Quiero Comprar',component:QuieroComprarComponent},
-  {path:'admin',title:'Administrador',component: AdminloginComponent},
-  {path:'admin/Ubicaciones',title:'Administrador-Ubicaciones', component:AdminUbicacionesComponent, canActivate: [loginGuard,agenteMerting]},
-  {path:'admin/Principal',title:'Principal-Admin',component:AdminprincipalComponent, canActivate: [loginGuard,agenteMerting]},
-  {path:'admin/Promociones',title:'Principal-Admin',component:PromocionesComponent, canActivate: [loginGuard,agenteMerting]},
   {path:'alert',title:'alert',component:AlertComponent},
   {path:'admin/suscripciones',title:'suscripciones',component:SuscripcionesComponent, canActivate: [loginGuard,agenteMerting]},
-  {path:'youtube',title:'youtube',component:YoutubeComponent},
-  {path:'admin/usuarios',title:'usuarios',component:UsuariosComponent, canActivate: [loginGuard,agenteMerting]},
   {path:'noticas',component:NoticiasComponent},
   {path:'nuestros-clientes',title:'PROCASA | Nuestros Clientes',component:NuestrosClientesComponent},
   {path:'productos',title:'PROCASA | Productos', component:ProductosComponent},
   {path:'unete-nuestro-equipo',title:'PROCASA | Únete a Nuestro Equipo',component:NuestroEquipoComponent},
-  {path:'admin/Productos', component:AdminproductosComponent, canActivate: [loginGuard,agenteMerting]},
-  {path:'admin/Equipo', component:EquipoComponent, canActivate: [loginGuard,agenteRH]},
   {path:'noticia',title:'PROCASA | Noticia',component:NoticiaUnicaComponent},
   {path:'noticia/:id', component:NoticiaUnicaComponent},
-  {path:'admin/generales', component:GeneralesComponent, canActivate: [loginGuard,agenteMerting]},
-  {path:'admin/nuestras-marcas', component:NuestrasMarcasComponent, canActivate: [loginGuard,agenteMerting]},
   {path:'informacion-plaza',title:'PROCASA | Información de Plaza',component:EquipoUnicoComponent},
   {path:'informacion-plaza/:id',title:'PROCASA | Información de Plaza',component:EquipoUnicoComponent},
-
   {path:'productos/:nuestras-marcas',title:'PROCASA | Nuestras Marcas',component:ProductosComponent},
   {path:'productos/:por-que-procasa',title:'PROCASA | ¿Por qué Procasa?',component:ProductosComponent},
   {path:'productos/:catalogo',title:'PROCASA | Catálogo',component:ProductosComponent},
@@ -61,7 +39,6 @@ const routes: Routes = [
   {path:'Inicio/:mision-vision-valores',title:'PROCASA | Misión, Visión y Valores',component:PrincipalComponent},
   {path:'Inicio/:noticias',title:'PROCASA | Noticias',component:PrincipalComponent},
   {path:'noti',title:'DELETE',component:NoticiasMovilComponent},
-  {path:'admin/nuestros-clientes',component:NuestrosClienteComponent},
   {path:'**',redirectTo:'Inicio', pathMatch:'full'}
 ]
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,7 @@ import { NavbarComponent } from './componentes/Complements/navbar/navbar.compone
 import { MisionValorComponent } from './componentes/Cliente/mision-valor/mision-valor.component';
 import { QuieroComprarComponent } from './componentes/Cliente/quiero-comprar/quiero-comprar.component';
 import { UbicacionesComponent } from './componentes/Cliente/ubicaciones/ubicaciones.component';
-import { AdminprincipalComponent } from './componentes/admin/adminprincipal/adminprincipal.component';
-import { AdminloginComponent } from './componentes/admin/adminlogin/adminlogin.component';
-import { AdminUbicacionesComponent } from './componentes/admin/admin-ubicaciones/admin-ubicaciones.component';
 import { MenuComponent } from './componentes/Complements/menu/menu.component';
-import { AdminNavbarComponent } from './componentes/admin/admin-navbar/admin-navbar.component';
-import { PromocionesComponent } from './componentes/admin/promociones/promociones.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FlechaComponent } from './componentes/Complements/flecha/flecha.component';
@@ -28,7 +23,6 @@ import { AlertComponent } from './componentes/Complements/alert/alert.component'
 import { SuscripcionesComponent } from './componentes/Cliente/suscripciones/suscripciones.component';
 import { YoutubeComponent } from './componentes/Complements/youtube/youtube.component';
 import { SafePipe } from './safe.pipe';
-import { UsuariosComponent } from './componentes/admin/usuarios/usuarios.component';
 import { NgOptimizedImage } from '@angular/common';
 import { NoticiasNEWComponent } from './componentes/Complements/noticias-new/noticias-new.component';
 import { MostrarPorUbiPipe } from './pipes/mostrar-por-ubi.pipe';
@@ -37,21 +31,16 @@ import { NuestrosClientesComponent } from './componentes/Cliente/nuestros-client
 import { ProductosComponent } from './componentes/Cliente/productos/productos.component';
 import { MostrarimgsUbicacionPipe } from './pipes/mostrarimgs-ubicacion.pipe';
 import { NuestroEquipoComponent } from './componentes/Cliente/nuestro-equipo/nuestro-equipo.component';
-import { AdminproductosComponent } from './componentes/admin/adminproductos/adminproductos.component';
-import { EquipoComponent } from './componentes/admin/equipo/equipo.component';
 import { BackgroundComponent } from './componentes/Complements/background/background.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MatIconModule } from '@angular/material/icon';
 import { NgxUiLoaderModule,NgxUiLoaderConfig } from 'ngx-ui-loader';
 import { NavbarWhiteComponent } from './componentes/Complements/navbar-white/navbar-white.component';
 import { NoticiaUnicaComponent } from './componentes/Cliente/noticia-unica/noticia-unica.component';
-import { GeneralesComponent } from './componentes/admin/generales/generales.component';
-import { NuestrasMarcasComponent } from './componentes/admin/nuestras-marcas/nuestras-marcas.component';
 import { EquipoUnicoComponent } from './componentes/Cliente/equipo-unico/equipo-unico.component';
 import { ScrollTopService } from './services/scroll.service';
 import { MostrarNombrePlazaPipe } from './pipes/mostrar-nombre-plaza.pipe';
 import { NoticiasMovilComponent } from './componentes/Cliente/noticias-movil/noticias-movil.component';
-import { NuestrosClienteComponent } from './componentes/admin/nuestros-cliente/nuestros-cliente.component';
 
 
 
@@ -67,19 +56,13 @@ import { NuestrosClienteComponent } from './componentes/admin/nuestros-cliente/n
     MisionValorComponent,
     QuieroComprarComponent,
     UbicacionesComponent,
-    AdminprincipalComponent,
-    AdminloginComponent,
-    AdminUbicacionesComponent,
     MenuComponent,
-    AdminNavbarComponent,
-    PromocionesComponent,
     FlechaComponent,
     FooterMainComponent,
     AlertComponent,
     SuscripcionesComponent,
     YoutubeComponent,
     SafePipe,
-    UsuariosComponent,
     NoticiasNEWComponent,
     MostrarPorUbiPipe,
     MostrarAliasPipe,
@@ -87,17 +70,12 @@ import { NuestrosClienteComponent } from './componentes/admin/nuestros-cliente/n
     ProductosComponent,
     MostrarimgsUbicacionPipe,
     NuestroEquipoComponent,
-    AdminproductosComponent,
-    EquipoComponent,
     BackgroundComponent,
     NavbarWhiteComponent,
     NoticiaUnicaComponent,
-    GeneralesComponent,
-    NuestrasMarcasComponent,
     EquipoUnicoComponent,
     MostrarNombrePlazaPipe,
     NoticiasMovilComponent,
-    NuestrosClienteComponent
   ],
   imports: [
     BrowserModule,
